9baa69614bb2e8b4d678e68b9085fbc6b91cdeb9
tmartin <theo.martin22@icloud.com>
2023-10-02 22:13:57 +0200

    Modify game

--- snake.js
+++ snake.js
@@ -1,135 +1,151 @@
 const canvas = document.getElementById("gameCanvas");
-    const context = canvas.getContext("2d");
-    const boxSize = 20;
-    let score = 0;
+const context = canvas.getContext("2d");
+const boxSize = 20;
+let score = 0;
 
-    const foods = [{
-            emoji: "🍆",
-            score: 10
-        },
-        {
-            emoji: "💦",
-            score: 15
-        },
-        {
-            emoji: "🍑",
-            score: 20
-        },
-        {
-            emoji: "🍒",
-            score: 25
-        },
-        {
-            emoji: "🍌",
-            score: 30
+const foods = [
+    {emoji: "🍆", score: 10},
+    {emoji: "💦", score: 15},
+    {emoji: "🍑", score: 20},
+    {emoji: "🍒", score: 25},
+    {emoji: "🍌", score: 30}
+];
+
+let currentFood = randomFood();
+
+function randomFood() {
+    const randomIndex = Math.floor(Math.random() * foods.length);
+    return foods[randomIndex];
+}
+
+const imagesArray = [
+    "images/girl-1/step-1.jpeg",
+    "images/girl-1/step-2.jpeg",
+    "images/girl-1/step-3.jpeg",
+    "images/girl-1/step-4.jpeg",
+    "images/girl-1/step-5.jpeg",
+    "images/girl-1/step-6.jpeg",
+    "images/girl-1/step-7.jpeg"
+];
+const imageElement = document.getElementById("sideImage");
+
+let snake = [{x: 5, y: 5}];
+let food = {x: Math.floor(Math.random() * 30), y: Math.floor(Math.random() * 30)};
+let dx = 1;
+let dy = 0;
+const scoreElement = document.getElementById("score");
+
+document.addEventListener("keydown", changeDirection);
+
+function changeDirection(e) {
+    if (e.keyCode == 37 && dx === 0) {dx = -1; dy = 0;}
+    if (e.keyCode == 38 && dy === 0) {dx = 0; dy = -1;}
+    if (e.keyCode == 39 && dx === 0) {dx = 1; dy = 0;}
+    if (e.keyCode == 40 && dy === 0) {dx = 0; dy = 1;}
+}
+
+function updateImage() {
+    imageElement.src = imagesArray[Math.floor(score / 100) % imagesArray.length];
+}
+
+let gameInterval;
+
+function checkCollisionWithSelf() {
+    for (let i = 1; i < snake.length; i++) {
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+            return true;
         }
-    ];
-
-    let currentFood = randomFood();
-
-    function randomFood() {
-        const randomIndex = Math.floor(Math.random() * foods.length);
-        return foods[randomIndex];
     }
+    return false;
+}
 
-    const imagesArray = [
-        "images/girl-1/step-1.jpeg",
-        "images/girl-1/step-2.jpeg",
-        "images/girl-1/step-3.jpeg",
-        "images/girl-1/step-4.jpeg",
-        "images/girl-1/step-5.jpeg",
-        "images/girl-1/step-6.jpeg",
-        "images/girl-1/step-7.jpeg"
+function gameOver() {
+    clearInterval(gameInterval); // Arrêtez le jeu
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = "50px Arial";
+    context.textAlign = "center";
+    context.fillText("GAME OVER", canvas.width / 2, canvas.height / 2);
+}
+
+function initializeGame() {
+    snake = [
+        { x: 5, y: 5 }
     ];
-    const imageElement = document.getElementById("sideImage");
-    let snake = [{
-        x: 5,
-        y: 5
-    }];
-    let food = {
+    score = 0;
+    dx = 1;
+    dy = 0;
+    food = {
         x: Math.floor(Math.random() * 30),
         y: Math.floor(Math.random() * 30)
     };
-    let dx = 1;
-    let dy = 0;
-    const scoreElement = document.getElementById("score");
+    currentFood = randomFood();
+    document.getElementById("restartButton").style.display = 'none';
+    gameInterval = setInterval(drawGame, 100);
+}
 
-    document.addEventListener("keydown", changeDirection);
+function gameOver() {
+    clearInterval(gameInterval); // Arrêtez le jeu
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = "50px Arial";
+    context.textAlign = "center";
+    context.fillText("GAME OVER", canvas.width / 2, canvas.height / 2);
+    document.getElementById("restartButton").style.display = 'block'; // Affiche le bouton
+}
 
-    function changeDirection(e) {
-        if (e.keyCode == 37 && dx === 0) {
-            dx = -1;
-            dy = 0;
-        } // Left
-        if (e.keyCode == 38 && dy === 0) {
-            dx = 0;
-            dy = -1;
-        } // Up
-        if (e.keyCode == 39 && dx === 0) {
-            dx = 1;
-            dy = 0;
-        } // Right
-        if (e.keyCode == 40 && dy === 0) {
-            dx = 0;
-            dy = 1;
-        } // Down
+document.getElementById("restartButton").addEventListener("click", function() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    initializeGame(); // Initialisez le jeu à nouveau
+});
+
+function drawGame() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Determine next position
+    const nextX = snake[0].x + dx;
+    const nextY = snake[0].y + dy;
+
+    // Handle wall passage
+    let head;
+    if (nextX < 0) {
+        head = {x: canvas.width / boxSize - 1, y: Math.floor(Math.random() * (canvas.height / boxSize))};
+    } else if (nextX > (canvas.width / boxSize) - 1) {
+        head = {x: 0, y: Math.floor(Math.random() * (canvas.height / boxSize))};
+    } else if (nextY < 0) {
+        head = {x: Math.floor(Math.random() * (canvas.width / boxSize)), y: canvas.height / boxSize - 1};
+    } else if (nextY > (canvas.height / boxSize) - 1) {
+        head = {x: Math.floor(Math.random() * (canvas.width / boxSize)), y: 0};
+    } else {
+        head = {x: nextX, y: nextY};
+    }
+    snake.unshift(head);
+
+    if (checkCollisionWithSelf()) {
+        gameOver();
+        return;
     }
 
-    function updateImage() {
-        imageElement.src = imagesArray[Math.floor(score / 100) % imagesArray.length];
+    // Check for food consumption
+    if (head.x === food.x && head.y === food.y) {
+        food = {x: Math.floor(Math.random() * 30), y: Math.floor(Math.random() * 30)};
+        score += currentFood.score;
+        scoreElement.innerText = "Score: " + score;
+        currentFood = randomFood();
+        updateImage();
+    } else {
+        snake.pop();
     }
 
-    function drawGame() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
+    // Draw snake
+    snake.forEach(part => {
+        context.fillStyle = "green";
+        context.fillRect(part.x * boxSize, part.y * boxSize, boxSize, boxSize);
+    });
 
-        // Check if the snake hits the border
-        if (snake[0].x < 0) {
-            dx = 1;
-            dy = 0;
-        } // Left border
-        if (snake[0].x > (canvas.width / boxSize) - 1) {
-            dx = -1;
-            dy = 0;
-        } // Right border
-        if (snake[0].y < 0) {
-            dx = 0;
-            dy = 1;
-        } // Top border
-        if (snake[0].y > (canvas.height / boxSize) - 1) {
-            dx = 0;
-            dy = -1;
-        } // Bottom border
+    // Draw food
+    context.font = "20px Arial";
+    context.fillText(currentFood.emoji, food.x * boxSize, (food.y + 1) * boxSize);
+}
 
-        // Move the snake
-        const head = {
-            x: snake[0].x + dx,
-            y: snake[0].y + dy
-        };
-        snake.unshift(head);
+updateImage();
+gameInterval = setInterval(drawGame, 100);
 
-        if (snake[0].x === food.x && snake[0].y === food.y) {
-            food = {
-                x: Math.floor(Math.random() * 30),
-                y: Math.floor(Math.random() * 30)
-            };
-            score += currentFood.score;
-            scoreElement.innerText = "Score: " + score;
-            currentFood = randomFood();
-            updateImage();
-        } else {
-            snake.pop();
-        }
-
-        // Draw snake
-        snake.forEach(part => {
-            context.fillStyle = "green";
-            context.fillRect(part.x * boxSize, part.y * boxSize, boxSize, boxSize);
-        });
-
-        // Draw food
-        context.font = "20px Arial";
-        context.fillText(currentFood.emoji, food.x * boxSize, (food.y + 1) * boxSize);
-    }
-
-    updateImage(); // to set the initial image based on the score
-    setInterval(drawGame, 100);
--- snaxe.php
+++ snaxe.php
@@ -1,9 +1,17 @@
 <?php include 'header.php'; ?>
 
-<p id="score">Score: 0</p>
 <div id="gameContainer">
     <canvas id="gameCanvas" width="600" height="600"></canvas>
     <img id="sideImage" src="images/girl-1/step-1.jpeg" alt="Une image tellement cool qu'elle a besoin de lunettes de soleil">
+</div>
+
+<div id="gameMenu">
+    <p id="score">Score: 0</p>
+    <button id="restartButton" style="display:none;">Restart</button>
+</div>
+
+<div id="game">
+    <script src="snake.js"></script>
 </div>
 
 </body>
