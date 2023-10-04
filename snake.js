0863cfc66ff1d9d4387be820ed4ef06579b78521
tmartin <theo.martin22@icloud.com>
2023-10-04 18:21:52 +0200

    Added an emoji ☠️ with a pop every 20 seconds, game over in case of collision

--- snake.js
+++ snake.js
@@ -8,6 +8,8 @@ let gameInterval;  // Declare gameInterval
 let bestScore = localStorage.getItem('bestScore') || 0;
 const bestScoreElement = document.getElementById("bestScore");
 bestScoreElement.innerText = "Best Score: " + bestScore;
+let skull = null;
+let skullTimeout = null;
 
 const grassImage = new Image();
 grassImage.src = "images/patterns/grass.png";
@@ -35,7 +37,7 @@ const imagesArray = [
 const imageElement = document.getElementById("sideImage");
 
 let snake = [{x: 5, y: 5}];
-let food = generateRandomPosition();
+let food = generateRandomPositionForFood();
 let dx = 1;
 let dy = 0;
 let isPaused = false;
@@ -47,8 +49,6 @@ function startGame() {
 
 function drawStartButton() {
     context.fillStyle = '#DDD'; // Couleur de fond du bouton
-    //context.fillRect(canvas.width / 4, canvas.height / 2 - 30, canvas.width / 2, 60); // Dessine le bouton
-
     context.fillStyle = '#666'; // Couleur du texte
     context.font = "30px 'Roboto-Light', sans-serif";
     context.textAlign = 'center';
@@ -59,7 +59,6 @@ canvas.addEventListener("click", function(event) {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-
     if (!gameStarted && x > canvas.width / 4 && x < (canvas.width / 4) * 3 && y > canvas.height / 2 - 30 && y < canvas.height / 2 + 30) {
         startGame();
     }
@@ -75,7 +74,8 @@ function initializeGame() {
     score = 0;
     dx = 1;
     dy = 0;
-    food = generateRandomPosition();
+    food = generateRandomPositionForFood();
+    generateRandomPositionForSkull();
     currentFood = randomFood();
     scoreElement.innerText = "Score: " + score;
     document.getElementById("restartButton").style.display = 'none';
@@ -87,12 +87,27 @@ function updateImage() {
     imageElement.src = imagesArray[Math.min(Math.floor(score / 100), imagesArray.length - 1)];
 }
 
-function generateRandomPosition() {
+function generateRandomPositionForFood() {
     return {
         x: Math.floor(Math.random() * (canvas.width / boxSize)),
         y: Math.floor(Math.random() * (canvas.height / boxSize))
-    };
+    }
 }
+
+function generateRandomPositionForSkull() {
+    if (!skull && Math.random() < 0.2) { // 20% chance and only if there's no existing skull
+        skull = {
+            x: Math.floor(Math.random() * (canvas.width / boxSize)),
+            y: Math.floor(Math.random() * (canvas.height / boxSize))
+        };
+        
+        // Remove the skull after 20 seconds
+        skullTimeout = setTimeout(() => {
+            skull = null;
+        }, 20000); // 20 seconds
+    }
+}
+
 
 function changeDirection(e) {
     if (e.key === 'ArrowLeft' && dx === 0) {dx = -1; dy = 0;}
@@ -129,23 +144,17 @@ function drawSnakePart(part) {
     const gradient = context.createLinearGradient(part.x * boxSize, part.y * boxSize, (part.x + 1) * boxSize, (part.y + 1) * boxSize);
     gradient.addColorStop(0, "limegreen"); // Début de gradient
     gradient.addColorStop(1, "green");    // Fin de gradient
-
     context.fillStyle = gradient;
     context.fillRect(part.x * boxSize, part.y * boxSize, boxSize, boxSize);
-
     context.shadowColor = '#666666';
     context.shadowBlur = 5;
     context.shadowOffsetX = 2;
     context.shadowOffsetY = 2;
-
     const cornerRadius = 5;
     context.lineJoin = "round";
     context.lineWidth = cornerRadius;
-
-    // Remplissage d'un rectangle arrondi
     context.strokeRect(part.x * boxSize + (cornerRadius / 2), part.y * boxSize + (cornerRadius / 2), boxSize - cornerRadius, boxSize - cornerRadius);
     context.fillRect(part.x * boxSize + (cornerRadius / 2), part.y * boxSize + (cornerRadius / 2), boxSize - cornerRadius, boxSize - cornerRadius);
-
     context.shadowBlur = 0;
     context.shadowOffsetX = 0;
     context.shadowOffsetY = 0;
@@ -157,7 +166,7 @@ document.addEventListener("keydown", function(e) {
     if (e.key === 'ArrowUp' && dy === 0) {dx = 0; dy = -1; e.preventDefault();}
     if (e.key === 'ArrowRight' && dx === 0) {dx = 1; dy = 0; e.preventDefault();}
     if (e.key === 'ArrowDown' && dy === 0) {dx = 0; dy = 1; e.preventDefault();}
-    
+
     // For pausing game
     if (e.key === " " || e.key === "Space") {  // We check for both possible values here
         e.preventDefault();
@@ -176,20 +185,16 @@ function togglePause() {
     }
 }
 
-
 function drawGame() {
     if(!gameStarted) {
         drawStartButton();
         return; // Si le jeu n'a pas démarré, on sort de la fonction après avoir dessiné le bouton
     }
-
     if (isPaused) {
         const pauseWindowWidth = canvas.width * 0.5;
         const pauseWindowHeight = canvas.height * 0.3;
         const x = (canvas.width - pauseWindowWidth) / 2;
         const y = (canvas.height - pauseWindowHeight) / 2;
-    
-    
         context.fillStyle = '#FFF';
         context.font = "150px 'Fuggles', sans-serif";
         context.textAlign = "center";
@@ -197,31 +202,27 @@ function drawGame() {
     
         return;
     }
-    
-    
-    
-    
     context.clearRect(0, 0, canvas.width, canvas.height);
-
     // Update snake position
     let head = {x: snake[0].x + dx, y: snake[0].y + dy};
-    
     // Handle wall collision
     if (head.x < 0) head.x = (canvas.width / boxSize) - 1;
     if (head.x > (canvas.width / boxSize) - 1) head.x = 0;
     if (head.y < 0) head.y = (canvas.height / boxSize) - 1;
     if (head.y > (canvas.height / boxSize) - 1) head.y = 0;
-
     snake.unshift(head);
-
+    generateRandomPositionForSkull();
     if (checkCollisionWithSelf()) {
         gameOver();
         return;
     }
-
     // Check for food consumption
     if (head.x === food.x && head.y === food.y) {
-        food = generateRandomPosition();
+        if (currentFood.emoji === "☠️") {
+            gameOver();
+            return;
+        }
+        food = generateRandomPositionForFood();
         score += currentFood.score;
         scoreElement.innerText = "Score: " + score;
         currentFood = randomFood();
@@ -229,19 +230,23 @@ function drawGame() {
     } else {
         snake.pop();
     }
-
     const pattern = context.createPattern(grassImage, 'repeat');
     context.fillStyle = pattern;
     context.fillRect(0, 0, canvas.width, canvas.height);
-
     // Draw snake
     snake.forEach(drawSnakePart);
-
     // Draw food
     context.font = "20px Arial";
     context.fillText(currentFood.emoji, food.x * boxSize, (food.y + 1) * boxSize);
+    // Draw skull if it exists
+    if (skull) {
+        context.fillText("☠️", skull.x * boxSize, (skull.y + 1) * boxSize);
+    }
+    if (skull && head.x === skull.x && head.y === skull.y) {
+        gameOver();
+        return;
+    }
 }
-
 
 document.getElementById("restartButton").addEventListener("click", function() {
     context.clearRect(0, 0, canvas.width, canvas.height);
